66deb1bfde1b1da265934f226a211e14c5299a74
Soner Adıaman <skozayy@gmail.com>
2023-02-15 02:44:41 +0300

    created side bar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,6 +39,24 @@ const Navbar = () => {
             </div>
           </div>
         </form>
+        <div className="text-xl p-3 bg-zinc-900 rounded-full">
+          <TiMicrophone />
+        </div>
+      </div>
+      <div className="flex gap-5 items-center text-xl ">
+        <BsCameraVideo />
+        <IoAppsSharp />
+        <div className="relative">
+          <BsBell />
+          <span className="absolute bottom-2 left-2 text-xs bg-red-600 rounded-full px-1">
+            9+
+          </span>
+        </div>
+        <img
+          src="https://yt3.ggpht.com/yti/AHXOFjXJ62GXqPMwc2CT06vh4XxqehhCh0jNl2imYgdizA=s88-c-k-c0x00ffffff-no-rj-mo"
+          alt="Logo"
+          className="w-9 h-9 rounded-full"
+        />
       </div>
     </div>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,15 @@
-import React from "react";
 import Navbar from "../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 const Home = () => {
   return (
-    <div>
-      <Navbar />
+    <div className="max-h-screen overflow-hidden">
+      <div style={{ height: "7.5vh" }}>
+        <Navbar />
+      </div>
+      <div className="flex" style={{ height: "92.5vh" }}>
+        <Sidebar />
+      </div>
     </div>
   );
 };
